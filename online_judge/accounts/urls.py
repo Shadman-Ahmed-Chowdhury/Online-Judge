dc9560ed37b1f21a65ae21480568cfd3f1c6e9e3
Shadman <shadmanchwodhury87@gmail.com>
2020-06-09 02:28:39 +0600

    Login and Signup page added

--- online_judge/accounts/urls.py
+++ online_judge/accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+	path('register/', views.registerPage, name="register"), 
+	path('login/', views.loginPage, name="login"),
 	path("", views.index, name='index'),
 ]
--- online_judge/accounts/views.py
+++ online_judge/accounts/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
 
 # Create your views here.
+
+def registerPage(request): 
+	form = CreateUserForm()
+
+	if request.method == 'POST': 
+		form = CreateUserForm(request.POST)
+		if form.is_valid(): 
+			form.save() 
+			return redirect('accounts:login')
+
+
+	context = {'form':form}
+	return render(request, 'accounts/register.html', context)
+
+def loginPage(request): 
+	context = {}
+	return render(request, 'accounts/login.html')
 
 def index(request): 
 	return render(request, 'accounts/index.html')
