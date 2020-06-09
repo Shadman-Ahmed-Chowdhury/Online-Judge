59877eb8cc422896ff18630c2bd81c72047ef60a
Shadman <shadmanchwodhury87@gmail.com>
2020-06-09 13:57:36 +0600

    Signup and Login Functionality added

--- online_judge/accounts/urls.py
+++ online_judge/accounts/urls.py
@@ -6,5 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
 	path('register/', views.registerPage, name="register"), 
 	path('login/', views.loginPage, name="login"),
+	path('logout/', views.logoutUser, name='logout'),
 	path("", views.index, name='index'),
 ]
--- online_judge/accounts/views.py
+++ online_judge/accounts/views.py
@@ -1,24 +1,49 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def registerPage(request): 
-	form = CreateUserForm()
+	if request.user.is_authenticated:
+		return redirect('accounts:index')
+	else: 
+		form = CreateUserForm()
 
-	if request.method == 'POST': 
-		form = CreateUserForm(request.POST)
-		if form.is_valid(): 
-			form.save() 
-			return redirect('accounts:login')
+		if request.method == 'POST': 
+			form = CreateUserForm(request.POST)
+			if form.is_valid(): 
+				form.save() 
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Sign Up successful for ' + user)
+				return redirect('accounts:login')
 
 
-	context = {'form':form}
-	return render(request, 'accounts/register.html', context)
+		context = {'form':form}
+		return render(request, 'accounts/register.html', context)
 
 def loginPage(request): 
-	context = {}
-	return render(request, 'accounts/login.html')
+	if request.user.is_authenticated:
+		return redirect('accounts:index')
+	else: 
+		if request.method == 'POST': 
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+			if user is not None: 
+				login(request, user)
+				return redirect('accounts:index')
+			else: 
+				messages.info(request, 'Username or Password is incorrect!')
+		context = {}
+		return render(request, 'accounts/login.html', context)
+
+def logoutUser(request): 
+	logout(request)
+	return redirect('accounts:login')
 
 def index(request): 
 	return render(request, 'accounts/index.html')
