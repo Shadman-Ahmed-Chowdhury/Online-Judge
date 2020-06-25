4df900bed8e2b4a9e6cca600a51707203965e488
Shadman <shadmanchowdhury87@gmail.com>
2020-06-26 01:37:40 +0600

    Blog Post Views Added

--- online_judge/blog/views.py
+++ online_judge/blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post 
 # Create your views here.
 
 def home(request):
-	context = {}
+	posts = Post.objects.all()
+	context = {'posts': posts}
 	return render(request, 'blog/home.html', context)
 
 def about(request):
