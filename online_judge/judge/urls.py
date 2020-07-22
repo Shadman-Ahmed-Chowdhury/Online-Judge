dcc583b1c4094d7f3cff7d9b16c048fdaa7400dd
Shadman <shadmanchowdhury87@gmail.com>
2020-07-22 12:18:03 +0600

    Update View and Delete View of problems added

--- online_judge/judge/urls.py
+++ online_judge/judge/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views 
-from .views import ProblemListView, ProblemCreateView, ProblemDetailView
+from .views import ProblemListView, ProblemCreateView, ProblemDetailView, ProblemUpdateView, ProblemDeleteView
 
 app_name = 'judge'
 
@@ -9,5 +9,7 @@ urlpatterns = [
 	path('problems/', ProblemListView.as_view(), name='problems-list'),
 	path('problem/new/', ProblemCreateView.as_view(), name='problem-create'), 
 	path('problem/<int:pk>/<code>/', ProblemDetailView.as_view(), name='problem-detail'),
+	path('problem/<int:pk>/update', ProblemUpdateView.as_view(), name='problem-update'),
+	path('problem/<int:pk>/delete', ProblemDeleteView.as_view(), name='problem-delete'), 
 	
 ]
--- online_judge/judge/views.py
+++ online_judge/judge/views.py
@@ -41,3 +41,30 @@ class ProblemCreateView(LoginRequiredMixin, CreateView):
 		form.instance.author = self.request.user
 		print(form.cleaned_data)
 		return super().form_valid(form)
+
+class ProblemUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	template_name = 'judge/problem_create.html'
+	form_class = ProblemModelForm
+	queryset = Problem.objects.all()
+
+	def form_valid(self, form): 
+		form.instance.author = self.request.user
+		#print(form.cleaned_data)
+		return super().form_valid(form)
+
+	def test_func(self): 
+		post = self.get_object()
+		if self.request.user == post.author: 
+			return True
+		return False
+
+class ProblemDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Problem 
+	success_url = reverse_lazy('judge:problems-list')
+	def test_func(self): 
+		post = self.get_object()
+		if self.request.user == post.author: 
+			return True
+		return False
+
+
